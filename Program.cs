a65efb35051fcd01927d58661e0e7b34df6dba9a
jgrissom <jgrissom91@gmail.com>
2021-10-17 19:13:49 -0500

    [1.02] NLog
    - dotnet add package NLog.Web.AspNetCore -v 4.14.0
    - Configure nlog targets for output
    - Create static nlog instance

--- Program.cs
+++ Program.cs
@@ -3,14 +3,23 @@
 // Version:     1.xx
 
 using System;
+using NLog.Web;
+using System.IO;
 
 namespace MediaLibrary
 {
     class Program
     {
+        // create static instance of Logger
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+
+            logger.Info("Program started");
+
             Console.WriteLine("Hello World!");
+
+            logger.Info("Program ended");
         }
     }
 }
