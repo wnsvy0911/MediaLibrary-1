568d5081831b63c3da1da2fe5ac35982515c19a7
jgrissom <jgrissom91@gmail.com>
2021-10-17 20:51:41 -0500

    [2.01] How many movies from 1990?
    - Where filter operator
    - Contains quantifier operator
    - Count aggregation method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
 using System;
 using NLog.Web;
 using System.IO;
+using System.Linq;
 
 namespace MediaLibrary
 {
@@ -20,6 +21,15 @@ namespace MediaLibrary
             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
             logger.Info(scrubbedFile);
             MovieFile movieFile = new MovieFile(scrubbedFile);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // LINQ - Where filter operator & Contains quantifier operator
+            var Movies = movieFile.Movies.Where(m => m.title.Contains("(1990)"));
+            // LINQ - Count aggregation method
+            Console.WriteLine($"There are {Movies.Count()} movies from 1990");
+
+            Console.ForegroundColor = ConsoleColor.White;
 
             logger.Info("Program ended");
         }
