06dd8dec26dbff1f557dadc43e870320dd87e10a
jgrissom <jgrissom91@gmail.com>
2021-10-17 19:10:36 -0500

    [1.01] Initial commit
    - dotnet new console -f net5.0

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Repository:  MediaLibrary
+// Author:      Jeff Grissom
+// Version:     1.xx
+
+using System;
+
+namespace MediaLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
