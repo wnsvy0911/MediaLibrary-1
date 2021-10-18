19dc5f51e9b23fb869ee07a96597c5e5d2a281eb
jgrissom <jgrissom91@gmail.com>
2021-10-17 19:14:49 -0500

    [1.03] Create Media class
    - Create mediaId, title, genres properties
    - Create Display method

---
+++ Media.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace MediaLibrary
+{
+    public class Media
+    {
+        // public properties
+        public UInt64 mediaId { get; set; }
+        public string title { get; set; }
+        public List<string> genres { get; set; }
+
+        // constructor
+        public Media()
+        {
+            genres = new List<string>();
+        }
+
+        // public method
+        public string Display()
+        {
+            return $"Id: {mediaId}\nTitle: {title}\nGenres: {string.Join(", ", genres)}\n";
+        }
+    }
+}
