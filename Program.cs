e2c13563258ca14f396b85be287651cc5022b816
jgrissom <jgrissom91@gmail.com>
2021-10-17 20:42:14 -0500

    [1.07] Album class
    - Create the Album class - it is a subclass of the Media class
    - Add artist and recordLabel properties to the Album class
    - Override the base class’s Display method in the Album class
    - Create an instance of the Album class in the Program and display it

--- Program.cs
+++ Program.cs
@@ -29,6 +29,16 @@ namespace MediaLibrary
 
             Console.WriteLine(movie.Display());
 
+            Album album = new Album
+            {
+                mediaId = 321,
+                title = "Greatest Album Ever, The (2020)",
+                artist = "Jeff's Awesome Band",
+                recordLabel = "Universal Music Group",
+                genres = { "Rock" }
+            };
+            Console.WriteLine(album.Display());
+
             logger.Info("Program ended");
         }
     }
