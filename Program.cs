5555d5578f7128c00efb3fc7d9148ef06d30060c
Junpyo Kim <jpkimkorea@gmail.com>
2022-03-25 12:04:57 -0500

    Home many Movies from 1921? - Where filter operator - Contains quantifier operator - Count aggregation method

--- Program.cs
+++ Program.cs
@@ -29,11 +29,12 @@ namespace MediaLibrary
             var Movies = movieFile.Movies.Where(m => m.title.Contains("(1990)"));
             // LINQ - Count aggregation method
             Console.WriteLine($"There are {Movies.Count()} movies from 1990");
-
             // LINQ - Any quantifier operator & Contains quantifier operator
             var validate = movieFile.Movies.Any(m => m.title.Contains("(1921)"));
             Console.WriteLine($"Any movies from 1921? {validate}");
-
+            // LINQ - Where filter operator & Contains quantifier operator & Count aggregation method
+            int num = movieFile.Movies.Where(m => m.title.Contains("(1921)")).Count();
+            Console.WriteLine($"There are {num} movies from 1921");
 
             Console.ForegroundColor = ConsoleColor.White;
 
